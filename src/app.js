5e4be82fba9a46f3f0e6cb167f28da7faadef78f
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-09-20 01:23:44 +0530

    dynamic route

--- src/app.js
+++ src/app.js
@@ -2,32 +2,19 @@ const express = require('express');
 
 const app = express();
 
-//by using app.get, it will only handle GET call to /user
-app.get("/user",(req,res)=>{
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({firstName: "Laksh", lastName: "Nijhawan"})
+// })
+
+
+//DYNAMIC ROUTES
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstName: "Laksh", lastName: "Nijhawan"})
 })
 
-//by using app.post, it will only handle POST call to /user
-app.post("/user",(req,res)=>{
-    res.send("Data saved successfully")
-})
 
-//this will match all the other HTTP method API calls to /route
-app.use("/route",(req,res)=>{
-    res.send("Hello from the route")
-});
-
-app.use("/hi/2",(req,res)=>{
-    res.send("hi x2")
-});
-
-app.use("/hi",(req,res)=>{
-    res.send("hi mate")
-});
-
-app.use("/",(req,res)=>{
-    res.send("welcome to server")
-});
 
 app.listen(1000,()=>{
     console.log("Server is accesible at above port")
