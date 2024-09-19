a6c811836dde3e21e4de3ad64c96794e4643fbb5
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-09-20 00:23:29 +0530

    exploring GET, POST, DELETE, PUT, PATCH API calls n testing them via POSTMAN

--- src/app.js
+++ src/app.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 const app = express();
 
+//by using app.get, it will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Laksh", lastName: "Nijhawan"})
+})
 
+//by using app.post, it will only handle POST call to /user
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully")
+})
+
+//this will match all the other HTTP method API calls to /route
 app.use("/route",(req,res)=>{
     res.send("Hello from the route")
+});
+
+app.use("/hi/2",(req,res)=>{
+    res.send("hi x2")
+});
+
+app.use("/hi",(req,res)=>{
+    res.send("hi mate")
+});
+
+app.use("/",(req,res)=>{
+    res.send("welcome to server")
 });
 
 app.listen(1000,()=>{
