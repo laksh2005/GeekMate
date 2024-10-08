db8b05f6948d3130632c5f445cd20cee1805b1a6
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-10-09 01:45:05 +0530

    error handlers

--- src/app.js
+++ src/app.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const app = express();
-const { adminAuth, userAuth } = require("./middlewares/auth")
 
-//Handle Auth middleware for all the GET, POST requests
-//This middleware makes sure that the data sending or the data deleting elements are accessible, only when the admin is authorized
-//or else, it tends to send the error and the code won't be access from there onwards
+app.get("/getUserData", (req,res)=>{
+    //Logic of DB call and get user data
+    //we will randomly generate errors, to see the concept of error handlers
 
-//this means that all the /admin calls come here first
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
+    throw new Error("thisisanerrorlol");
+    res.send("User data sent");
 
-//THIS ISN'T AUTHORIZED
-app.get("/user/login", (req,res)=>{
-    res.send("User login function")
+    // an alternative method using TRY & CATCH
+    // try{
+    //     throw new Error("thisisanerrorlol");
+    //     res.send("User data sent");
+    // }
+    // catch{
+    //     res.status(500).send("Something went wrong!");
+    // }
+
+});
+
+//(err,req,res,next)
+//in this order, express treats first one as error, seocnd as request, third as response, and 4th one as next
+app.use("/", (err,req,res,next)=>{
+    if (err) {
+        res.status(500).send("Something went wrong!");
+    }
 })
-
-//THIS IS AUTHORIZED
-app.get("/user/data", userAuth, (req,res)=>{
-    res.send("User data sent")
-})
-
-app.get("/admin/getAllData", (req,res)=>{
-    res.send("All data is sent")
-})
-
-app.get("/admin/deleteAllData", (req,res)=>{
-    res.send("All data is gone")
-})
+//also, always write this error handling file at the end
 
 app.listen(1000,()=>{
     console.log("Server is accessible at localhost:1000")
