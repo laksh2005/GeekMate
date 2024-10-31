5aaa525a319f762571af19405aac4ea8cf76fb3d
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-11-01 01:36:10 +0530

    finding the user data entered in /login via  GET /profile API, by using JWT and cookies

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const userModel = require('./models/user');
 const { ReturnDocument } = require('mongodb');
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 //this is a readymade middleware, which helps us read the JSON data from the end user, and converts it to JS
 app.use(express.json());
+app.use(cookieParser());
 
 //we store the data in the database using post api request
 app.post("/signup",async (req,res)=>{
@@ -40,7 +43,7 @@ app.post("/signup",async (req,res)=>{
 app.post("/login",async (req,res)=>{
     try{
         const {emailId, password} = req.body;
-        // findinf the email in db
+        // finding the email in db
         const user = await User.findOne({emailId: emailId});
 
         if(!user){
@@ -50,6 +53,12 @@ app.post("/login",async (req,res)=>{
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if(isPasswordValid){
+            //HERE WE WILL CREATE A JWT TOKEN
+            //ONLY IF THE PASSWORD ID VALID AND THE LOGIN HAPPENS
+            const token = await jwt.sign({_id: user._id}, "Thisis+key+forthistoken123");
+
+            //Adding the token to cookie and then sending a response back to the user
+            res.cookie("token", token);
             res.send("Login successful");
         }
         else{
@@ -57,6 +66,29 @@ app.post("/login",async (req,res)=>{
         }
     } catch(err){
         res.status(400).send("ERR : " + err.message);
+    }
+})
+
+app.get("/profile", async (req,res)=>{
+    try{
+        const cookies = req.cookies;
+        const { token } = cookies;
+
+        if(!token){
+            throw new Error("Invalid Token");
+        }
+    
+        const decodedMessage = await jwt.verify(token, "Thisis+key+forthistoken123");
+        const { _id } = decodedMessage;
+        const user = await User.findById(_id);
+
+        if(!user){
+            throw new Error("USER DOES NOT EXIST");
+        }
+
+        res.send(user);
+    } catch(err){
+        res.status(400).send("Error :" + err.message);
     }
 })
 
