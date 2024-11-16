e560221994cbb0ef189984e7caa01cf9a9d2e88f
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-11-16 17:54:22 +0530

    GET api for /user/requests/connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,7 +13,7 @@ userRouter.get("/user/requests/recieved", userAuth, async (req,res)=>{
         const connectionRequests = await ConnectionRequest.find({
             toUserID: new mongoose.Types.ObjectId(loggedInUser._id),
             status: "interested"
-        }).populate("fromUserID", ["firstName", "lastName"]);
+        }).populate("fromUserID", ["firstName", "lastName", "age", "gender", "skills"]);
         
 
         res.json({
@@ -26,4 +26,34 @@ userRouter.get("/user/requests/recieved", userAuth, async (req,res)=>{
     }
 });
 
-module.exports = userRouter;
+//API to get all the connections of the loggedInUser
+ userRouter.get("/user/requests/connections", userAuth, async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {toUserID: loggedInUser._id, status: "accepted"},
+                {fromUserID: loggedInUser._id, status: "accepted"},
+            ],
+        }).populate("fromUserID", ["firstName", "lastName", "age", "gender", "skills"] )
+          .populate("toUserID", ["firstName", "lastName", "age", "gender", "skills"])
+
+          //here we populated both to and from user, since one of them would be the logged in user obviously, when we check the connections which are accepted
+
+          //now here, if the fromuser is the loggedinuser, then we will map the row (user details) of touserid (since it would be from's connection) & VICE VERSA
+          const data = connectionRequests.map((row)=>{
+            if(row.fromUserID._id.toString() === loggedInUser._id.toString()){
+                return row.toUserID;
+            }
+            return row.fromUserID;
+          })
+
+          res.json({data})
+
+    } catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+ module.exports = userRouter;
