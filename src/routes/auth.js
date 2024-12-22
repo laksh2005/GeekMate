93213359c5d2db367324dd7b251bff2927eceeef
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2024-12-22 19:33:11 +0530

    feature to automatically login a new signed up user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -26,8 +26,15 @@ authRouter.post("/signup",async (req,res)=>{
             password: passwordHash,
         });
 
-        await user.save();
-        res.send("User Added successfully")
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8 * 3600000)
+        });
+
+        res.json({message: "User Added Successfully!", data:savedUser})
+
     } catch (err){
         res.status(400).send("error saving the user: " + err.message);
     }
